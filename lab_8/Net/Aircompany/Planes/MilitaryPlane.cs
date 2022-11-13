b10858c849ce108dec81a2020847779b67aadf72
DeiServus <113282269+DeiServus@users.noreply.github.com>
2022-11-14 00:12:09 +0300

    Add files via upload

---
+++ lab_8/Net/Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,44 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        public MilitaryType _militaryType;
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType militaryType)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            _militaryType = militaryType;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var plane = obj as MilitaryPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   _militaryType == plane._militaryType;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + _militaryType.GetHashCode();
+            return hashCode;
+        }
+
+        public MilitaryType PlaneTypeIs()
+        {
+            return _militaryType;
+        }
+
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", militaryType=" + _militaryType +
+                    '}');
+        }        
+    }
+}
---
+++ lab_8/Net/Aircompany/Program.cs
@@ -0,0 +1,26 @@
+﻿using Aircompany.Models;
+using Aircompany.Planes;
+using System;
+using System.Collections.Generic;
+
+namespace Aircompany
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Airport airport = new Airport(PlanesList.Planes);
+            Airport militaryAirport = new Airport(airport.GetMilitaryPlanes());
+            Airport passengerAirport = new Airport(airport.GetPassengersPlanes());
+            Console.WriteLine(militaryAirport
+                              .SortPlanesByMaxFlightDistance()
+                              .ToString());
+            Console.WriteLine(passengerAirport
+                              .SortPlanesByMaxSpeed()
+                              .ToString());
+            Console.WriteLine(passengerAirport.GetPassengerPlaneWithMaxPassengersCapacity());
+        }
+    };
+       
+    }
+
